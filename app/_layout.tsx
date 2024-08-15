3883d6aac95d009efbf787772879052ab7b48ff0
Alfred D'Souza <dsouzaalfred@gmail.com>
2024-08-15 16:28:54 +0100

    add bottom navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { theme } from "../theme";
+import { Entypo, Feather } from "@expo/vector-icons";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
