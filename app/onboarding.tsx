b7827450963401eebe5ec24407ebf20c3a35bacc
Alfred D'Souza <dsouzaalfred@gmail.com>
2024-08-15 18:47:48 +0100

    Add image

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  useWindowDimensions,
+  View,
+  Text,
+} from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "@/components/PlantlyImage";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -22,7 +29,12 @@ export default function OnboardingScreen() {
       colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
       style={styles.container}
     >
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagline}>Keep Your Plants Happy and Hydrated</Text>
+      </View>
       <StatusBar style="light" />
+      <PlantlyImage />
       <PlantlyButton title="Let me in!" onPress={handlePress} />
     </LinearGradient>
   );
@@ -31,8 +43,21 @@ export default function OnboardingScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
+    padding: 24,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: "center",
   },
 });
